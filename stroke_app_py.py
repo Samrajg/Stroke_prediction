1185a56c2f54c501ccd00f1a781c1f5d510d85bb
Godwin Samraj G <samrajgodwin7@gmail.com>
2025-06-17 23:32:56 +0530

    Update stroke_app_py.py

--- stroke_app_py.py
+++ stroke_app_py.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1LiNKazL3Qe2ulPwQMYYKPEQ1ixJiS95r
 """
 
-!pip install streamlit
 
 import streamlit as st
 import pandas as pd
